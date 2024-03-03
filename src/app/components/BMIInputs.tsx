a95b3b313241a0157526c09fe58303d8bbfa7e5a
Mrlion <mrlion4202@gmail.com>
2024-03-03 11:39:30 +0000

    changed color if inputs, some other changes

--- src/app/components/BMIInputs.tsx
+++ src/app/components/BMIInputs.tsx
@@ -4,7 +4,7 @@ import { RiMailFill, RiReactjsLine } from "react-icons/ri";
 const BMIInputs = () => {
   return (
     <div className="bg-gray-1000 text-white p-4 rounded-lg shadow-md">
-      <label htmlFor="weight" className="block mb-2 font-poppins">
+      <label htmlFor="weight" className="block mb-1 font-poppins">
         Weight (kg):
       </label>
       <input
@@ -13,7 +13,7 @@ const BMIInputs = () => {
         id="weight"
         name="weight"
         placeholder="Enter your weight"
-        className="w-full p-2 bg-gray-900 text-white rounded-md mb-4"
+        className="w-full p-2 bg-zinc-900 text-white rounded-md mb-4"
       />
       <label htmlFor="height" className="block mb-2.5">
         Height (cm):
@@ -24,7 +24,7 @@ const BMIInputs = () => {
         id="height"
         name="height"
         placeholder="Enter your height"
-        className="w-full p-2 bg-gray-900 text-white rounded-md mb-4"
+        className="w-full p-2 bg-zinc-900 text-white rounded-md mb-4"
       />
       <button className="bg-yellow-500 hover:bg-yellow-530 text-white font-bold py-2 px-5 rounded flex justify-between lg:justify-around items-center mx-auto">
         <RiMailFill style={{ width: "20px", height: "20px", margin: "2px" }} />
--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,20 +1,12 @@
 import React from "react";
-import { twMerge } from "tailwind-merge";
 import { RiCalculatorFill } from "react-icons/ri";
 
 export function Navbar() {
   return (
-    <nav className="flex justify-between lg:justify-around py-8 px-4 lg:px-0">
-      <h3
-        className={twMerge(
-          "p-2",
-          "text-white font-bold text-xl flex items-center",
-          "transition-colors duration-200",
-          "hover:text-yellow-500 hover:cursor-pointer"
-        )}
-      >
+    <nav className="flex justify-between lg:justify-around py-6 px-4 lg:px-0">
+      <h3 className="p-2 text-white font-bold text-xl flex items-center transition-colors duration-200 hover:text-yellow-500 hover:cursor-pointer">
         <RiCalculatorFill className="mr-2" />
-        <span>LionBMICalculator</span>
+        <span>Lion&apos;s BMI Calculator</span>
       </h3>
     </nav>
   );
